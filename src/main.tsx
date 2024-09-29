2b3b3c39c4cb97c69a480f44ce9d46bd0de1f8fb
BENKIRANE Asmae - CDS <a.benkirane@cat-amania.com>
2024-09-29 17:21:25 +0200

    Premier commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,51 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AudioPlayer } from "./components/AudioPlayer";
+import "./components/css/styles.css"; // Importer le fichier CSS
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AudioPlayer 
-      src="audios/audio.webm"
-      minimal={false}
-      width={350}
-      trackHeight={75}
-      barWidth={1}
-      gap={1}
-
-      visualise={true}
-      backgroundColor="#FFF8DE"
-      barColor="#C1D0B5"
-      barPlayedColor="#99A98F"
-
-      skipDuration={2}
-      showLoopOption={true}
-      showVolumeControl={true}
-      onended={() => console.log("ENDED")}
-      // seekBarColor="purple"
-      // volumeControlColor="blue"
-      // hideSeekBar={true}
-      // hideTrackKnobWhenPlaying={true}
-    />
-    <br />
-    <AudioPlayer 
-      src="audios/test.mp3"
-      minimal={true}
-      width={250}
-      trackHeight={40}
-      barWidth={3}
-      gap={1}
-
-      visualise={true}
-      backgroundColor="#FFF8DE"
-      barColor="#C1D0B5"
-      barPlayedColor="#99A98F"
-
-      skipDuration={2}
-      showLoopOption={true}
-      showVolumeControl={true}
-
-      hideSeekBar={true}
-      hideSeekKnobWhenPlaying={true}
-    />
+    <div className="audio-page-container">
+      <div className="audio-player-container">
+        <AudioPlayer
+          src="audios/YaqoutEtMarjane_ManGhachana.m4a"
+          minimal={false}
+          width={350}
+          trackHeight={75}
+          barWidth={1}
+          gap={1}
+          visualise={true}
+          backgroundColor="#FFF8DE"
+          barColor="#C1D0B5"
+          barPlayedColor="#99A98F"
+          skipDuration={2}
+          showLoopOption={true}
+          showVolumeControl={true}
+          onended={() => console.log("ENDED")}
+        />
+      </div>
+      <div className="image-container">
+        <img
+          src="/images/couverture_YaqoutEtMarjane_ManGhachana.png"
+          alt="Background"
+        />
+      </div>
+    </div>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,11 @@
-import { defineConfig } from 'vite'
-import dts from "vite-plugin-dts"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [react()],
   build: {
-    copyPublicDir: false,
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'AudioPlayer',
-      fileName: (format) => `react-audio-player-component.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-        }
-      }
-    },
+    copyPublicDir: true,
+    outDir: "dist", // Assure-toi que le dossier de sortie est "dist"
   },
-})
-
+});
